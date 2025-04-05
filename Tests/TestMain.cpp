ced4022fc5b9dcacfaa7e6c3bea27c9ec8455961
artem <ArtemKorolev24092009@yandex.ru>
2025-04-05 21:15:40 +0300

    Make one cmake lists.txt and add some tests

--- Tests/TestMain.cpp
+++ Tests/TestMain.cpp
@@ -1,8 +1,38 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
+#include <string>
+#include "../App/Include/FileSystemEntity.hpp"
+#include "../App/Include/FileSystemAnalyzer.hpp"
+#include "../App/Include/Directory.hpp"
+#include "../App/Include/Utils.hpp"
+#include "../App/Include/OsSpecificConfig.hpp"
 
 
-TEST_CASE("Simple test to ensure Catch2 is working") {
-    REQUIRE(1 + 1 == 2);
-    REQUIRE_FALSE(1 + 1 == 3);
+TEST_CASE("Get full path worked test case") {
+    const auto excpected = "path" + OsSpecificConfig::getPathSeparator() + "name";
+    
+    FileSystemEntity entity("path", "name");
+    auto real = entity.getFullPath();
+    REQUIRE(excpected == real);
+    
+    FileSystemEntity entity2("path/", "name");
+    real = entity2.getFullPath();
+    REQUIRE(excpected == real);
+}
+
+TEST_CASE("Entity exists method test case") {
+#ifdef _WIN32
+    const std::string homePath = "C:\\";
+#else
+    const std::string homePath = "/";
+#endif
+    const auto real = FileSystemAnalyzer::isEntityExists(std::make_shared<Directory>(homePath, ""));
+
+    REQUIRE(real == true);
+}
+
+TEST_CASE("Ends with function test case") {
+    REQUIRE(utils::endsWith("asdfg", "fg") == true);
+    REQUIRE_FALSE(utils::endsWith("", "fg") == true);
+    REQUIRE_FALSE(utils::endsWith("asdfg", "s") == true);
 }
